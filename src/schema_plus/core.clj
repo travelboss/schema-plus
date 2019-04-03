7f405f65416d5c765651508692c7ebcb4a67bfae
Tyler Hobbs <tyler@tylerlhobbs.com>
2019-04-03 16:18:31 -0500

    Initial implementation of defschema+, tests, etc

---
+++ src/schema_plus/core.clj
@@ -0,0 +1,207 @@
+(ns schema-plus.core
+  "Core functions, including defschema+"
+  (:require [clojure.set :refer [difference]]
+            [clojure.string :as string]
+            [schema.core :as s]
+            [schema-generators.generators :as sg]
+            [clojure.test.check.generators :as cg]))
+
+(def generator-registry
+  "Holds a map of Schema -> Generator for default generation behavior."
+  (atom {}))
+
+(defn generate
+  "Generate a value from a schema."
+  [schema-obj]
+  (sg/generate schema-obj @generator-registry))
+
+(defn get-generator-for
+  "Get the current clojure.test.check.generators.Generator for a schema."
+  [schema-obj]
+  (get @generator-registry schema-obj))
+
+(defn set-generator-for
+  "Manually set the Generators for some schema object. Normally this
+   should only be used for raw Java Classes and other objects where
+   defschema+ can't be used directly."
+  [schema-obj generator]
+  (swap! @generator-registry assoc schema-obj generator))
+
+(defn process-opts
+  "Only for internal usage"
+  [kvs]
+  (when (odd? (count kvs))
+    (throw (RuntimeException. "Expected an even number of key/values")))
+
+  (let [opts-map (apply hash-map kvs)
+        extra-opts (-> opts-map
+                       keys
+                       set
+                       (difference #{:docs :generator :example :make-builders?}))]
+    (if (seq extra-opts)
+      (throw (RuntimeException. (str "Got unexpected option keys: "
+                                     (string/join ", " extra-opts))))
+      opts-map)))
+
+(defmacro defschema+
+  "Use this instead of defschema to get mock generation capabilities,
+   default example values for Swagger, and optional builder functions.
+
+   A docstring (and documentation for usage in Swagger) can be supplied by
+   passing an optional `:docs` argument followed by the string.
+
+   Based on the types used in the schema definition, a `clojure.test.check.generators.Generator`
+   instance will be created that can generate mock data satisfying the schema
+   definition. This automatically handles any referenced schemas that were also
+   created with defschema+.
+
+   The default generator behavior can be replaced or augmented by supplying a `:generator`
+   option. The following argument should either be a `clojure.test.check.generators.Generator`
+   instance (to totally replace the default generator) or a function accepting one argument. If
+   a function is passed, it will be passed as the first argument to `clojure.test.check.generators/fmap`,
+   with the default generator as the second argument. In other words, when generating, the output
+   of the default generator will be passed to your custom function as the only argument.
+
+   Example of supplying a custom generator:
+
+     (defschema+ Person
+       {:name schema/Str
+        :age schema/Int}
+       :generator
+       (gen/return {:name \"Bob\", :age 42}))
+
+   Example of supplying an fmap function:
+
+     (defschema+ Person
+       {:name schema/Str
+        :age schema/Int}
+       :generator
+       (fn [default-generated] (assoc default-generated :age 42)))
+
+   If :make-builders? is not supplied, or is followed by `true`, then default builder functions
+   will be defined. This will automatically define the following functions in the current
+   namespace (assuming the schema-name is `Foo` and there is a field named `:bar`):
+
+   (+Foo) - Create an initial, empty instance
+   (+Foo m) - Create an initial instance populated with fields populated from a map
+
+   (+Foo-with-bar this v) - set the `:bar` field on a Foo instance
+
+   (+Foo-build this) - Finalize creation of a Foo instance
+
+   (+Foo-mock-generate) - Generates a complete mock instance of Foo
+
+   Typical usage for creating a new instance would look like:
+
+     (-> (+Foo)
+         (+Foo-with-bar 123)
+         (+Foo-build))
+
+   Full example with all options:
+
+     (require '[schema-plus.core :refer [defschema+]])
+     (require '[schema.core :as schema])
+     (require '[clojure.test.check.generators :as gen])
+
+     (defschema+ Person
+       {:name schema/Str
+        :age schema/Int}
+       :doc \"A person with a name and age\"
+       :generator (gen/hash-map :name gen/char-alphanumeric
+                                :age gen/nat)
+       :example {:name \"Bob\", :age 42}
+       :make-builders? false)"
+
+  [schema-name schema-form & kvs]
+
+  (when (odd? (count kvs))
+    (throw (RuntimeException. "Expected an even number of key/values")))
+
+  (let [opts-map (process-opts kvs)
+        docstring (:docs opts-map "")
+        generator-customizer (:generator opts-map)
+        make-builders? (:make-builders? opts-map true)
+
+        ; build a seq of [setter-fn-name field-name] for all fields
+        fn-names-and-keys (if (and make-builders? (map? schema-form))
+                            (for [k (keys schema-form)]
+                              (let [field-name (if (seq? k) ; remove s/optional-key wrappers, etc
+                                                 (last k)
+                                                 k)]
+                                [(symbol (str "+" schema-name "-with-" (name field-name)))
+                                 field-name]))
+                            [])
+        builder-fn-name (symbol (str "+" schema-name))
+        build-fn-name (symbol (str "+" schema-name "-build+"))]
+
+    (cons
+      'do
+
+      (concat
+
+        (if make-builders?
+          ; for each field name, defn a setter function
+          (for [[fn-name schema-key] fn-names-and-keys]
+            `(defn ~fn-name
+               [this# v#]
+               (assoc this# ~schema-key v#)))
+          '())
+
+        (list
+          `(let [; make the normal defschema call
+                 schema-var# (s/defschema ~schema-name ~docstring ~schema-form)
+                 schema-obj# (var-get schema-var#)
+
+                 ; a naive generator simply based on types
+                 basic-generator# (sg/generator schema-obj# @generator-registry)
+
+                 generator# (cond
+                              ; nothing passed in, use default generator behavior
+                              (nil? ~generator-customizer)
+                              basic-generator#
+
+                              ; generator passed, use directly
+                              (cg/generator? ~generator-customizer)
+                              ~generator-customizer
+
+                              ; function passed, make new generator with fmap
+                              (fn? ~generator-customizer)
+                              (cg/fmap ~generator-customizer basic-generator#)
+
+                              :else
+                              (throw (RuntimeException. (str "Got bad generator argument for: " ~schema-name))))
+
+                 example# (if (contains? ~opts-map :example)
+                            (:example ~opts-map)
+                            (cg/generate generator#))
+
+                 schema-with-meta# (vary-meta
+                                     schema-obj#
+                                     assoc :json-schema {:description ~docstring
+                                                         :example example#})
+
+                 ; redef with metadata - not sure how else to properly do this
+                 schema-var# (s/defschema ~schema-name ~docstring schema-with-meta#)
+                 schema-obj# (var-get schema-var#)]
+
+             (s/validate schema-obj# example#)
+
+             ; update the registry with the new generator
+             (swap! generator-registry assoc schema-obj# generator#)
+
+             (when ~make-builders?
+               ; define function to start building instances
+               (defn ~builder-fn-name
+                 ([] ; from scratch
+                  {::incomplete true})
+                 ([m#] ; from an initial map
+                  (assoc m# ::incomplete true)))
+
+               ; define function to finish building instances
+               (defn ~build-fn-name
+                 [this#]
+                 (let [final# (dissoc this# ::incomplete)]
+                   (s/validate schema-obj# final#)
+                   final#)))
+
+             schema-obj#))))))
---
+++ test/schema_plus/core_test.clj
@@ -0,0 +1,232 @@
+(ns schema-plus.core-test
+  (:require [clojure.set :as sets]
+            [clojure.test :refer [deftest is testing]]
+            [clojure.test.check.generators :as cg]
+            [schema.core :as s]
+            [schema-plus.core :refer [defschema+ generate process-opts]])
+  (:import [java.util Date UUID]))
+
+(deftest options-processing-test
+  (testing "Bad args"
+    (is (thrown? RuntimeException (process-opts [:generator])))
+    (is (thrown? RuntimeException (process-opts [:badkey 123])))
+    (is (thrown? RuntimeException (process-opts [:docs "Whatever" :badkey 123])))))
+
+(deftest metadata-test
+  (testing "Doc metadata is attached correctly"
+    (defschema+ WithDocs
+      {:a s/Int}
+      :docs "The Docs")
+    (is (= "The Docs" (-> WithDocs meta :json-schema :description)))
+    (defschema+ WithoutDocs
+      {:a s/Int})
+    (is (= "" (-> WithoutDocs meta :json-schema :description))))
+
+  (testing "Example metadata is attached correctly"
+    (defschema+ WithExample
+      {:a s/Int}
+      :example {:a 123})
+    (is (= {:a 123} (-> WithExample meta :json-schema :example)))
+    (defschema+ WithoutExample
+      {:a s/Int})
+    (is (= [:a] (-> WithExample meta :json-schema :example keys)))))
+
+(deftest default-generator-test
+  (testing "Map With Simple Types"
+    (defschema+ MyMap
+      {:int s/Int
+       :str s/Str
+       :bool s/Bool
+       :num s/Num})
+    (let [gen (generate MyMap)]
+      (is (= #{:int :str :bool :num} (-> gen keys set)))
+      (is (integer? (:int gen)))
+      (is (string? (:str gen)))
+      (is (contains? #{true false} (:bool gen)))
+      (is (number? (:num gen)))))
+
+  (testing "Map With Java Class Types"
+    (defschema+ MyJavaMap
+      {:uuid UUID
+       :date Date})
+    (let [gen (generate MyJavaMap)]
+      (is (instance? UUID (:uuid gen)))
+      (is (instance? Date (:date gen)))))
+
+  (testing "Referential Map Types"
+    (defschema+ MyMapA
+      {:a s/Int
+       :b s/Str})
+    (defschema+ MyMapB
+      {:x s/Int
+       :y MyMapA})
+    (let [gen (generate MyMapB)]
+      (is (= #{:x :y} (-> gen keys set)))
+      (is (integer? (:x gen)))
+      (is (map? (:y gen)))
+      (is (integer? (-> gen :y :a)))
+      (is (string? (-> gen :y :b)))))
+
+  (testing "Simple Vector Types"
+    (defschema+ MyInts [s/Int])
+    (is (every? integer? (generate MyInts)))
+    (defschema+ MyStrs [s/Str])
+    (is (every? string? (generate MyStrs))))
+
+  (testing "Referential Vector Types"
+    (defschema+ MyMapC
+      {:a s/Int
+       :b s/Str})
+    (defschema+ MyVector [MyMapC])
+    (let [gen (generate MyVector)]
+      (is (every? map? gen))
+      (is (every? #(-> % :a integer?) gen))
+      (is (every? #(-> % :b string?) gen))))
+
+  (testing "Map With Optional Keys"
+    (defschema+ MyOptionalMap
+      {:a s/Int
+       (s/optional-key :b) s/Str})
+    (s/validate MyOptionalMap {:a 1})
+    (s/validate MyOptionalMap {:a 1 :b "foo"})
+    (let [gen (generate MyOptionalMap)]
+      (is (empty? (-> gen keys set (sets/difference #{:a :b}))))
+      (is (integer? (:a gen)))
+      (is (or (nil? (:b gen))
+              (string? (:b gen))))))
+
+  (testing "Map With Maybe Value"
+    (defschema+ MyMaybeMap
+      {:a s/Int
+       :b (s/maybe s/Str)})
+    (s/validate MyMaybeMap {:a 1 :b nil})
+    (s/validate MyMaybeMap {:a 1 :b "foo"})
+    (let [gen (generate MyMaybeMap)]
+      (is (= #{:a :b} (-> gen keys set)))
+      (is (integer? (:a gen)))
+      (is (or (nil? (:b gen))
+              (string? (:b gen))))))
+
+  (testing "Map With Constrained Value"
+    (defschema+ MyConstrainedValueMap
+      {:a s/Int
+       :b (s/constrained s/Str #(not (nil? %)))})
+    (s/validate MyConstrainedValueMap {:a 1 :b "foo"})
+    (let [gen (generate MyConstrainedValueMap)]
+      (is (= #{:a :b} (-> gen keys set)))
+      (is (integer? (:a gen)))
+      (is (string? (:b gen)))))
+
+  (testing "Fully Constrained Map"
+    (defschema+ MyConstrainedMap
+      (s/constrained
+        {:a s/Int
+         :b s/Str}
+        (constantly true)))
+    (s/validate MyConstrainedMap {:a 1 :b "foo"})
+    (let [gen (generate MyConstrainedMap)]
+      (is (= #{:a :b} (-> gen keys set)))
+      (is (integer? (:a gen)))
+      (is (string? (:b gen)))))
+
+  (testing "Constrained Simple Value"
+    (defschema+ MyConstrainedInt
+      (s/constrained s/Int integer?))
+    (s/validate MyConstrainedInt 1)
+    (is (integer? (generate MyConstrainedInt))))
+
+(testing "Enums"
+  (defschema+ MyEnum
+    (s/enum :foo :bar :baz))
+  (s/validate MyEnum :foo)
+  (is (contains? #{:foo :bar :baz} (generate MyEnum)))))
+
+(deftest test-generator-customization
+  (testing "Full Generator Replacement"
+    (defschema+ MyCustomGenMap
+      {:a s/Int
+       :b s/Str}
+      :generator
+      (cg/return {:a 123 :b "foo"}))
+    (is (= {:a 123 :b "foo"} (generate MyCustomGenMap)))
+
+    (defschema+ Zipcode
+      (s/constrained s/Str #(re-matches #"^\d{5}$" %))
+      :generator
+      (cg/return "90210"))
+    (is (= "90210" (generate Zipcode))))
+
+  (testing "Generator FMap Function"
+    (defschema+ MyCustomFMap
+      {:a s/Int
+       :b s/Str}
+      :generator
+      #(assoc % :a 123))
+    (let [gen (generate MyCustomFMap)]
+      (is (= 123 (:a gen)))
+      (is (string? (:b gen)))))
+
+  (testing "Nested FMap Customization"
+    (defschema+ MyCustomFMapInner
+      {:a s/Int
+       :b s/Str}
+      :generator
+      #(assoc % :a 123))
+    (defschema+ MyCustomFMapOuter
+      {:a MyCustomFMapInner
+       :b s/Str}
+      :generator
+      #(assoc % :b (-> % :a :b)))
+    (let [gen (generate MyCustomFMapOuter)]
+      (is (= 123 (-> gen :a :a)))
+      (is (string? (:b gen)))
+      (is (= (:b gen) (-> gen :a :b))))))
+
+; These declarations aren't necessary, but make Joker happy. These would
+; normally be avoided by properly importing from another namespace where
+; the schema is declared.
+(declare +MyPerson)
+(declare +MyPerson-with-name)
+(declare +MyPerson-with-age)
+(declare +MyPerson-build+)
+
+(deftest test-builder-functions
+  (defschema+ MyPerson
+    {:name s/Str
+     :age s/Int
+     (s/optional-key :occupation) s/Str})
+
+  (testing "Build with all mandatory fields"
+    (is (= {:age 42 :name "Bob"}
+           (-> (+MyPerson)
+               (+MyPerson-with-name "Bob")
+               (+MyPerson-with-age 42)
+               (+MyPerson-build+)))))
+
+  (testing "Build with all fields"
+    (is (= {:age 42 :name "Bob" :occupation "Builder"}
+           (-> (+MyPerson)
+               (+MyPerson-with-name "Bob")
+               (+MyPerson-with-age 42)
+               (+MyPerson-with-occupation "Builder")
+               (+MyPerson-build+)))))
+
+  (testing "Missing a mandatory field results in error"
+    (is
+      (thrown? Throwable
+               (-> (+MyPerson)
+                   (+MyPerson-with-age 42)
+                   (+MyPerson-build+)))))
+
+  (testing "Failing to finalize means schema validation will fail"
+    (is
+      (thrown? Throwable
+               (s/validate
+                 MyPerson
+                 (-> (+MyPerson)
+                     (+MyPerson-with-age 42)
+                     (+MyPerson-with-name "Bob"))))))
+  (testing "An initial map can be passed in"
+    (is (= {:name "Bob" :age 42}
+           (-> (+MyPerson {:name "Bob" :age 42})
+               (+MyPerson-build+))))))
