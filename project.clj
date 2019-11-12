4667037fdc715f88805a3857d7a4dd34813290d7
Nick Bailey <nickmbailey@gmail.com>
2019-11-12 12:10:49 -0600

    Let schema defitions be exported in clojurescript. (#2)

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject travelboss/schema-plus "1.0.5"
+(defproject travelboss/schema-plus "1.0.6"
   :description "Adds easy mock generation and builder functions to plumatic/schema definitions"
   :url "http://github.com/travelboss/schema-plus"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [prismatic/schema "1.1.7"]
+                 [prismatic/schema "1.1.12"]
                  [prismatic/schema-generators "0.1.2"]]
   :deploy-repositories [["releases" :clojars]])
